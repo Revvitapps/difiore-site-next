2ecb402162bef46836df314bb8f821d49f8c929b
Matthew Bryan <Matthew@revvit.io>
2025-11-12 11:01:59 -0500

    Adjust mobile background position in theme

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,7 +1,7 @@
 export const heroBackground = {
   imageSrc: "/difiore-hero-spotlight-house.png",
   positions: {
-    mobile: "50% 8%",
+    mobile: "25% 8%",
     tablet: "50% 6%",
     desktop: "50% 4%",
   },
